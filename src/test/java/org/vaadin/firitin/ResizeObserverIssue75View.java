9e0efd9c7cc0d2ab87131c5a0158e85482d0de94
Matti Tahvonen <matti@vaadin.com>
2024-11-14 20:14:42 +0200

    Added test case for #75

---
+++ src/test/java/org/vaadin/firitin/ResizeObserverIssue75View.java
@@ -0,0 +1,28 @@
+package org.vaadin.firitin;
+
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.router.Route;
+import org.vaadin.firitin.components.orderedlayout.VVerticalLayout;
+import org.vaadin.firitin.util.ResizeObserver;
+
+@Route
+public class ResizeObserverIssue75View extends VVerticalLayout {
+
+    public ResizeObserverIssue75View() {
+
+        var size = new Paragraph("UI size unknown (not attached yet)");
+        add(size);
+
+        var paragraph = new Paragraph("Lazy attached paragraph");
+
+        ResizeObserver.get()
+                .observe(paragraph, d -> size.setText(d.toString()));
+
+        add(new Button("Attach paragraph", e -> {
+            add(paragraph);
+        }));
+
+    }
+
+}
